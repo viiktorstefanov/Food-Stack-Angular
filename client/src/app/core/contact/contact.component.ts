9bb050634483c2d72d90d8107ce15c3930b5a361
Viktor Stefanov <victorystefanov@gmail.com>
2024-07-24 21:28:31 +0300

    contacts mobile styles

--- client/src/app/core/contact/contact.component.ts
+++ client/src/app/core/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  isMobileView: boolean = false;
 
+  ngOnInit(): void {
+    this.checkWindowSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.checkWindowSize();
+  }
+
+  checkWindowSize(): void {
+    const width = window.innerWidth;
+    this.isMobileView = width >= 360 && width <= 414;
+  }
 }
