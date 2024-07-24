38459f54978e0044d53bfc6429be178b7e838cc1
Viktor Stefanov <victorystefanov@gmail.com>
2024-07-24 19:39:23 +0300

    login mobile styles

--- client/src/app/shared/home-footer/home-footer.component.ts
+++ client/src/app/shared/home-footer/home-footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home-footer',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './home-footer.component.css'
 })
 export class HomeFooterComponent {
+
+  @Input() showFooter: boolean | undefined;
+
   currentYear: number = new Date().getFullYear();
+  
 }
