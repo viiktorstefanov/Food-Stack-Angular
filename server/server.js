cadcca7bd9b971d688dc2377b6b620324e97fc2e
Viktor Stefanov <victorystefanov@gmail.com>
2024-07-26 15:22:51 +0300

    deployment config

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios'); 
 const { databaseConfig } = require('./config/database');
 const expressConfig = require('./config/express');
 const routesConfig = require('./config/routes');
@@ -11,8 +12,22 @@ async function start() {
     expressConfig(app);
     routesConfig(app);
 
+    app.get('/ping', (req, res) => {
+        res.send('Server is alive');
+    });
+
     const PORT = process.env.PORT || 3000;
+    const HOST = process.env.HOST;
+    
     app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
+
+
+    setInterval(() => {
+        axios.get(`${HOST}/ping`)
+            .then(response => console.log(`Keep-alive ping response: ${response.data}`))
+            .catch(err => console.error('Error in keep-alive ping:', err));
+    }, 14 * 60 * 1000);
+
 }
 
 start();
